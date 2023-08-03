ed0382d0a2af335756605bb7a2a26f3abdb1e045
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2023-08-03 21:47:30 +0330

    Django Forms, email, send_mail

--- c1_blog_application/blog/urls.py
+++ c1_blog_application/blog/urls.py
@@ -6,8 +6,9 @@ app_name = "blog"
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     path(
-        "<int:year>/<int:month>/<int:day>/<slug:slug>",
+        "<int:year>/<int:month>/<int:day>/<slug:slug>/",
         views.post_detail,
         name="post_detail",
     ),
+    path("<int:post_id>/share/", views.post_share, name="post_share"),
 ]
--- c1_blog_application/blog/views.py
+++ c1_blog_application/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+
 from .models import Post
+from .forms import EmailPostForm
+from mysite.settings import DEFAULT_FROM_EMAIL
 
 
 def post_list(r):
@@ -27,3 +31,23 @@ def post_detail(r, year, month, day, slug):
         publish__day=day,
     )
     return render(r, "blog/post/detail.html", {"post": post})
+
+
+def post_share(r, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if r.method == "POST":
+        form = EmailPostForm(r.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+
+            post_url = r.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            msg = f"read post {post.title} at {post_url} \n\n"
+
+            send_mail(subject, msg, DEFAULT_FROM_EMAIL, [cd["to"]])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(r, "blog/post/share.html", {"post": post, "form": form, "sent": sent})
