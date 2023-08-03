0b51345201f17186f259823fb9b1dc41db89ee66
Erfan Rezaee <rezaee.erfan.2000@gmail.com>
2023-08-03 18:28:35 +0330

    pagination, EmptyPage, PageNotAnInteger, template include

--- c1_blog_application/blog/views.py
+++ c1_blog_application/blog/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
 def post_list(r):
     posts = Post.published.all()
-    return render(r, "blog/post/list.html", {"posts": posts})
+    paginator = Paginator(posts, 1)
+
+    page_number = r.GET.get("page", 1)
+    try:
+        posts_page = paginator.page(page_number)
+    except EmptyPage:
+        posts_page = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts_page = paginator.page(1)
+    return render(r, "blog/post/list.html", {"posts_page": posts_page})
 
 
 def post_detail(r, year, month, day, slug):
